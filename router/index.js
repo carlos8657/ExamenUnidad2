8790cae3ca8f5e0d03e21894c7d71301f05c5f63
carlos8657 <carlos865@outlook.com>
2023-03-06 17:05:15 -0700

    Cambiar extension

---
+++ router/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require ('body-parser');
+
+
+router.get('/CFE',(req,res)=>{
+    const valores ={
+        numBoleto: req.query.numBoleto,
+        destino: req.query.destino,
+        nombreCliente: req.query.nombreCliente,
+        edad: req.query.edad,
+        tipoBoleto: req.query.tipo,
+        precioBoleto: req.query.precioBoleto,
+        subtotal: req.query.subtotal,
+        impuesto: req.query.impuesto,
+        descuento: req.query.descuento,
+        total: req.query.total
+
+    }
+    res.render('compañialuz.html',valores)
+
+})
+
+router.post('/CFE',(req,res)=>{
+    const valores ={
+        numBoleto: req.body.numBoleto,
+        destino: req.body.destino,
+        nombreCliente: req.body.nombreCliente,
+        edad: req.body.edad,
+        tipoBoleto: req.body.tipo,
+        precioBoleto: req.body.precioBoleto,
+        subtotal: req.body.subtotal,
+        impuesto: req.body.impuesto,
+        descuento: req.body.descuento,
+        total: req.body.total
+
+    }
+    res.render('compañialuz.html', valores)
+})
+
+
+
+module.exports=router;
